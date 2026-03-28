5d35b4df086268f7dbeb7067be249dfc53c08e94
Parv Khatri <parv1711@gmail.com>
2026-03-28 10:33:42 +0530

    my first commit

---
+++ fstring.py
@@ -0,0 +1,28 @@
+#F-String was introduced in Python 3.6, and 
+# is now the preferred way of formatting strings.
+
+
+# age = 19
+# txt = f"My age is {age} years old"
+# print(txt)
+
+
+# Placeholders and modifiers
+
+# A placeholder can contain variables, operations, functions, 
+# and modifiers to format the value.
+
+
+# price = 49
+# txt = f"The price of the coke is {price} rupees"
+# print(txt)
+
+
+# price = 49
+# txt = f"The price of the coke is {price:.2f} rupees"
+# print(txt)
+
+
+
+txt = f"The price of the Red Wine is {34 * 77} rupees"
+print(txt)
