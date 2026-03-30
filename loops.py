59488efdfdc136dd4ccf3fb7e4a892ef0486be5b
Parv Khatri <parv1711@gmail.com>
2026-03-30 12:06:21 +0530

    Did some loops today

---
+++ loops.py
@@ -0,0 +1,18 @@
+# i = 0
+
+# while i != 0:
+#     print("woof")
+#     i = i - 1
+
+# while i < 3:
+#     print("woof")
+#     i += 1
+
+
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("woof")
